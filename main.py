f0fb922d3f9b39cb991b0b73de0f0286bd751d28
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-05-04 19:47:05 +0300

    Add blank methods for mandatory features

--- main.py
+++ main.py
@@ -2,34 +2,52 @@ from collections import UserDict
 
 
 class Field:
-    def __init__(self, value):
+    def __init__(self, value) -> None:
         self.value = value
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value)
 
 
 class Name(Field):
-    # реалізація класу
-    pass
+    ...
 
 
 class Phone(Field):
-    # реалізація класу
-    pass
+    def valid(self, phone: str) -> bool:
+        ...
 
 
 class Record:
-    def __init__(self, name):
+    def __init__(self, name) -> None:
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+    def add_phone(self, phone: str) -> None:
+        ...
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+    def remove_phone(self, phone: str) -> None:
+        ...
+
+    def edit_phone(self, current: str, new: str) -> None:
+        ...
+
+    def find_phone(self, phone: str) -> Phone:
+        ...
+
+    def __str__(self) -> str:
+        return ('Contact name: ',
+                self.name.value,
+                ', phones: ',
+                '; '.join(p.value for p in self.phones))
 
 
 class AddressBook(UserDict):
-    # реалізація класу
-    pass
+    def add_record(self, record: Record) -> None:
+        ...
+
+    def find(self, name: str) -> Record:
+        ...
+
+    def delete(self, name: str) -> None:
+        ...
