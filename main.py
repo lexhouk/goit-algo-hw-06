b2f73598c8677eb1ae427988217d688f7927e837
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-05-05 18:56:05 +0300

    Implement methods

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class Name(Field):
 
 
 class Phone(Field):
-    def valid(self, phone: str) -> bool:
-        ...
+    def valid(self) -> bool:
+        return len(str(self)) == 10 and str(self).isdigit()
 
 
 class Record:
@@ -24,30 +24,47 @@ class Record:
         self.phones = []
 
     def add_phone(self, phone: str) -> None:
-        ...
+        new = Phone(phone)
+
+        if new.valid():
+            self.phones.append(new)
 
     def remove_phone(self, phone: str) -> None:
-        ...
+        for id, current in enumerate(self.phones):
+            if str(current) == phone:
+                del self.phones[id]
+                break
 
     def edit_phone(self, current: str, new: str) -> None:
-        ...
+        new: Phone = Phone(new)
+
+        if new.valid():
+            for id, phone in enumerate(self.phones):
+                if str(phone) == current:
+                    self.phones[id] = new
+                    break
 
     def find_phone(self, phone: str) -> Phone:
-        ...
+        for current in self.phones:
+            if str(current) == phone:
+                return current
+
+        return Phone('')
 
     def __str__(self) -> str:
-        return ('Contact name: ',
-                self.name.value,
-                ', phones: ',
-                '; '.join(p.value for p in self.phones))
+        phones = '; '.join(str(phone) for phone in self.phones)
+        return f'Contact name: {str(self.name)}, phones: {phones}'
 
 
 class AddressBook(UserDict):
     def add_record(self, record: Record) -> None:
-        ...
+        self.data[str(record.name)] = record
 
     def find(self, name: str) -> Record:
-        ...
+        return self.data.get(name, Record(''))
 
     def delete(self, name: str) -> None:
-        ...
+        for id, record in self.data.items():
+            if str(record.name) == name:
+                del self.data[id]
+                break
