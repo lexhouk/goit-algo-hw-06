ead404941497a58353cc7350eb90a0b1ef0e8f66
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-05-10 17:45:37 +0300

    1. Make all attributes public 2. Move phone number validation to the constructor 3. Use direct access to attributes instead of using type conversion

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from dataclasses import dataclass
 
 @dataclass
 class Field:
-    _value: str
+    value: str
 
     def __str__(self) -> str:
-        return str(self._value)
+        return str(self.value)
 
 
 class Name(Field):
@@ -15,54 +15,58 @@ class Name(Field):
 
 
 class Phone(Field):
-    @staticmethod
-    def valid(phone: str) -> bool:
-        return len(phone) == 10 and phone.isdigit()
+    def __init__(self, value: str):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError('The number is not valid!')
+        super().__init__(value)
 
 
 class Record:
     def __init__(self, name: str) -> None:
         self.name = Name(name)
-        self.__phones: list[Phone] = []
+        self.phones: list[Phone] = []
 
     def __find(self,
                phone: str,
                get_instance: bool = False
                ) -> int | Phone | None:
-        for id, instance in enumerate(self.__phones):
-            if str(instance) == phone:
+        for id, instance in enumerate(self.phones):
+            if instance.value == phone:
                 return instance if get_instance else id
         return None
 
     def add_phone(self, phone: str) -> None:
-        if Phone.valid(phone):
-            self.__phones.append(Phone(phone))
+        self.phones.append(Phone(phone))
 
     def remove_phone(self, phone: str) -> None:
         if id := self.__find(phone):
-            del self.__phones[id]
+            del self.phones[id]
 
     def edit_phone(self, current: str, new: str) -> None:
-        if Phone.valid(new) and (id := self.__find(current)):
-            self.__phones[id]._value = new
+        id = self.__find(current)
+
+        if id is None:
+            raise ValueError(f'Phone {current} not found!')
+        else:
+            self.phones[id] = Phone(new)
 
     def find_phone(self, phone: str) -> Phone | None:
         return self.__find(phone, True)
 
     def __str__(self) -> str:
-        return f'Contact name: {str(self.name)}, ' \
-               f"phones: {'; '.join(map(str, self.__phones))}"
+        return f'Contact name: {self.name.value}, ' \
+               f"phones: {'; '.join(map(str, self.phones))}"
 
 
 class AddressBook(UserDict):
     def add_record(self, record: Record) -> None:
-        self.data[str(record.name)] = record
+        self.data[record.name.value] = record
 
     def find(self, name: str) -> Record | None:
         return self.data.get(name)
 
     def delete(self, name: str) -> None:
         for id, record in self.data.items():
-            if str(record.name) == name:
+            if record.name.value == name:
                 del self.data[id]
                 break
